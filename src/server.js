be56ab763e98031f88859f1e9c8f54213ca8668f
이동찬 <58020759+ehdcks3421@users.noreply.github.com>
2022-01-24 16:33:13 +0900

    Status Code, User DB Create

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter.js";
+import rootRouter from "./routers/rootRouter.js";
 import videoRouter from "./routers/videoRouter.js";
 import userRouter from "./routers/userRouter.js";
 
@@ -11,7 +11,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); //form 데이터를 들고오는 역할
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
