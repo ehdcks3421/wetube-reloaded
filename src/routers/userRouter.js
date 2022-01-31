56af42f1c1ef931e9509d78bdcbad03a7a848a7d
이동찬 <58020759+ehdcks3421@users.noreply.github.com>
2022-02-01 04:02:46 +0900

    Change Password

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,6 +6,8 @@ import {
   see,
   startGithubLogin,
   finishGithubLogin,
+  getChangePassword,
+  postChangePassword,
 } from "../controllers/userController";
 import { protectMiddleware, publicOnlyMiddleware } from "../middlewares";
 
@@ -16,6 +18,11 @@ const userRouter = express.Router();
 
 userRouter.get("/logout", protectMiddleware, logout);
 userRouter.route("/edit").all(protectMiddleware).get(getEdit).post(postEdit);
+userRouter
+  .route("/change-password")
+  .all(protectMiddleware)
+  .get(getChangePassword)
+  .post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
